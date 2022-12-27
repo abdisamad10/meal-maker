def0db999b3a34fc1b505925712f15f3f01ff48e
Abdisamad Mohamad <99802647+babanka@users.noreply.github.com>
2022-12-27 18:03:36 +0300

    Update meal_maker.js

--- folder/meal_maker.js
+++ folder/meal_maker.js
@@ -1,1 +1,31 @@
+//meal maker is a restuant website that makes meal and price each morning today's special.
+const menu = {
+_meal : '',
+_price: 0,
+
+set meal (mealToCheck) {
+  if (typeof mealToCheck === "string") {
+    return this._meal = mealToCheck;
+  }
+},
+
+set price (priceToCheck) {
+  if (typeof priceToCheck === 'number') {
+    return this._meal = priceToCheck;
+  }
+},
+
+   get todaysSpecial () {
+     if( this._meal && this._price){
+       return `today's meal is ${this._meal} for ${this._price}!.`
+     }else{
+       return 'Meal or price was not set correctly!.'
+     }
+}
+};
+
+menu._meal = 'geel';
+menu._price = '$5' ;
+console.log(menu.todaysSpecial);
+
 
